b2c67bf1e7b05f178f3be9021141f54695bbc53d
Emil Z <istarnightstudios@gmail.com>
2025-09-06 00:19:13 -0700

    GOAP IDK

--- MazeNavagationGame/Assets/GOAP/AgentBelief.cs
+++ MazeNavagationGame/Assets/GOAP/AgentBelief.cs
@@ -5,19 +5,10 @@ using System.Linq;
 using Sirenix.OdinInspector;
 
 
-public enum BeliefType
-{
-    MyLocation,
-    SeesPointOfInterest,
-    MovingToPointOfInterest,
-    Healthy
-}
-
 //Interface for ODIN Serialization for inspector visual
 public interface IBelief
 {
     string key { get; }
-    BeliefType type { get; }
     object boxedData { get; set; }
     float Confidence { get; set; }
     float timeStamp { get; set; }
@@ -25,27 +16,6 @@ public interface IBelief
     void UpdatePriorities(IReadOnlyList<IBelief> beliefs);
 }
 
-
-[Serializable]
-public abstract class Belief<T> : IBelief
-{
-    public string key { get; }
-
-    [ShowInInspector] public BeliefType type { get; set; }
-    public object boxedData { get => data; set => data = (T[])value; }
-    [ShowInInspector]  public float Confidence { get; set; }
-    public float timeStamp { get; set; }
-
-    public T[] data;
-    public abstract T UpdatePriorities(List<Belief<T>> beliefs);
-
-    public void UpdatePriorities(IReadOnlyList<IBelief> beliefs)
-    {
-        UpdatePriorities(beliefs);
-        timeStamp = Time.time;
-    }
-
-}
 
 public class BeliefStates<T>
 {
@@ -75,6 +45,31 @@ public class BeliefStates<T>
 }
 
 
+[Serializable]
+public abstract class Belief<T> : IBelief
+{
+    public string key { get; }
+    public object boxedData { get => data; set => data = (T[])value; }
+    [ShowInInspector] public float Confidence { get; set; }
+    public float timeStamp { get; set; }
+
+    public T[] data;
+    public abstract T[] UpdatePriorities(List<Belief<T>> beliefs);
+
+    public void UpdatePriorities(IReadOnlyList<IBelief> beliefs)
+    {
+        var interfaceBeliefsToTypeBeliefs = new List<Belief<T>>();
+
+        foreach (var b in beliefs)
+            if (b is Belief<T> beliefTyped)
+                interfaceBeliefsToTypeBeliefs.Add(beliefTyped);
+
+        data = UpdatePriorities(interfaceBeliefsToTypeBeliefs);
+        timeStamp = Time.time;
+    }
+
+}
+
 
 [Serializable]
 public class AgentBelief<T> : Belief<T>
@@ -82,28 +77,30 @@ public class AgentBelief<T> : Belief<T>
     protected GoapAgent agent;
     public string Name { get; }
 
-    Func<T> valueFunc;
-
     public AgentBelief() { }
     protected AgentBelief(string name) => Name = name;
 
-    public override T UpdatePriorities(List<Belief<T>> beliefs) => valueFunc();
+    public override T[] UpdatePriorities(List<Belief<T>> beliefs) => data;
 }
 
 public interface IBeliefFunctionality<GoapAgent, T>
 {
-    public abstract void Do(GoapAgent agent, T[] data);
+    public abstract T[] Do(GoapAgent agent, T[] data);
 
 }
 
 class BeliefMyLocation : IBeliefFunctionality<GoapAgent, Vector3>
 {
-    public void Do(GoapAgent agent, Vector3[] data) => SetMyLocation(agent, data);
-    Vector3 SetMyLocation(GoapAgent agent, Vector3[] data)
+    private readonly Vector3[] buffer = new Vector3[1];
+
+    public Vector3[] Do(GoapAgent agent, Vector3[] data) => SetMyLocation(agent, data);
+    Vector3[] SetMyLocation(GoapAgent agent, Vector3[] data)
     {
-        return agent.transform.position;
+        buffer[0] = agent.transform.position;
+        return buffer;
     }
 }
+
 
 //class IBeliefSeesPointOfInterest : IBeliefFunctionality
 //{
@@ -122,43 +119,23 @@ public class LocationBelief : AgentBelief<Vector3>
 {
     [SerializeReference] public IBeliefFunctionality<GoapAgent, Vector3> functionality;
 
-    Func<Vector3> location = () => Vector3.zero;
+    public Vector3[] Location => data;
+
 
     public LocationBelief() { }
 
-    public LocationBelief(string name, Func<Vector3> observedLocation) : base(name)
+    public LocationBelief(string name, Vector3[] data) : base(name)
     {
-        this.location = observedLocation;
+        this.data = data;
     }
-
-    public Vector3 Location => location();
     public bool InRangeOf(Vector3 pos, float range) => Vector3.Distance(agent.transform.position, pos) < range;
 
-    public override Vector3 UpdatePriorities(List<Belief<Vector3>> beliefs)
+    public override Vector3[] UpdatePriorities(List<Belief<Vector3>> beliefs)
     {
-        return location();
+        data = functionality.Do(agent, data);
+        return Location;
     }
 
-    public bool UpdatePriorities(List<Belief<Vector3>> beliefs, float dist)
-    {
-        
-
-        return InRangeOf(agent.transform.position, dist);
-    }
-
-
-    //Incr Confidence
-    bool SeesPointOfInterest()
-    {
-
-        return false;
-    }
-
-    //Incr Confidence
-    bool MovingToPointOfInterest()
-    {
-        return false;
-    }
 }
 
 
